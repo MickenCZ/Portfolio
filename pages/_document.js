9fc40fc6044b5403d4d4807d4a5fec9ff84267ce
MickenCZ <73470371+MickenCZ@users.noreply.github.com>
2025-12-10 20:54:32 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,9 +10,7 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" /> 
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet" />
-        <meta name="description" content="Portfolio of Michael Cirkl" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="author" content="Michael Cirkl" />
         <meta name="keywords" content="Portfolio, React, Next.js" />
       </Head>
       <body>
@@ -21,4 +19,5 @@ export default function Document() {
       </body>
     </Html>
   )
+
 }
